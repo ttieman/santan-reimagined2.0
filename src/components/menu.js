4a57287c8f686244e5c8af729885f4958b5b665f
ttieman <tiemantanner@gmail.com>
2023-02-24 14:39:28 -0700

    added navbar to menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Hero, Navbar, Card, Tabs, Container, Tile, Image, Block, Columns, Sizes } from "react-bulma-components";
-
+import PageNav from "./navbar";
 import 'bulma/css/bulma.min.css';
 
 const Menu = () => {
     return (
         <div>
+            <PageNav />
+        
+        <div>
           <p>hello</p>  
+        </div>
         </div>
     );
 };
