b975cd9345767f221b8aee01318e330637beda83
ttieman <tiemantanner@gmail.com>
2023-02-24 14:30:57 -0700

    initial imports for menu

---
+++ src/components/menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Hero, Navbar, Card, Tabs, Container, Tile, Image, Block, Columns, Sizes } from "react-bulma-components";
+
+import 'bulma/css/bulma.min.css';
+
+const Menu = () => {
+    return (
+        <div>
+          <p>hello</p>  
+        </div>
+    );
+};
+
+export default Menu;
