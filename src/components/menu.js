572b34263530516a70a81634cd1074a9faac1a4b
ttieman <tiemantanner@gmail.com>
2023-02-24 15:06:21 -0700

    implemented boiler plate tiles for development

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,16 +1,63 @@
 import React from "react";
-import { Hero, Navbar, Card, Tabs, Container, Tile, Image, Block, Columns, Sizes } from "react-bulma-components";
+import { Hero, Navbar, Card, Tabs, Heading, Container,Notification, Tile, Image, Block, Box, Section, Columns, Sizes } from "react-bulma-components";
 import PageNav from "./navbar";
 import 'bulma/css/bulma.min.css';
 
 const Menu = () => {
     return (
         <div>
+
             <PageNav />
-        
-        <div>
-          <p>hello</p>  
-        </div>
+
+           
+
+                <Section>
+                    <Box>
+                        <Tile kind="ancestor">
+                            <Tile size={8} vertical>
+                                <Tile>
+                                    <Tile kind="parent" vertical>
+                                        <Tile kind="child" renderAs={Notification} color="primary">
+                                            <Heading>Vertical...</Heading>
+                                            <Heading subtitle>Top tile</Heading>
+                                        </Tile>
+                                        <Tile kind="child" renderAs={Notification} color="warning">
+                                            <Heading>Tiles...</Heading>
+                                            <Heading subtitle>Bottom Tile...</Heading>
+                                        </Tile>
+                                    </Tile>
+                                    <Tile kind="parent">
+                                        <Tile kind="child" renderAs={Notification} color="info">
+                                            <Heading>Middle Tile...</Heading>
+                                            <Heading subtitle>With image Tile...</Heading>
+                                            <Image
+                                                size="4by3"
+                                                src="http://bulma.io/images/placeholders/640x480.png"
+                                            />
+                                        </Tile>
+                                    </Tile>
+                                </Tile>
+                                <Tile kind="parent">
+                                    <Tile kind="child" renderAs={Notification} color="danger">
+                                        <Heading>Wide tile</Heading>
+                                        <Heading subtitle>Aligned with the right tile</Heading>
+                                        <div className="content" />
+                                    </Tile>
+                                </Tile>
+                            </Tile>
+                            <Tile kind="parent">
+                                <Tile kind="child" renderAs={Notification} color="success">
+                                    <div className="content">
+                                        <Heading>Tall tile</Heading>
+                                        <Heading subtitle>With even more content</Heading>
+                                        <div className="content" />
+                                    </div>
+                                </Tile>
+                            </Tile>
+                        </Tile>
+                    </Box>
+                </Section>
+            
         </div>
     );
 };
