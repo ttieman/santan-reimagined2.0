0b1663609176abeefbb3b550ce04a2bdfb3cb0f2
quetc_ <carter@epicpro.co>
2023-02-24 13:35:32 -0800

    routing update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" />
+        <Route path="/menu" element={ <Menu />}/>
       </Routes>
     </Router>
   );
