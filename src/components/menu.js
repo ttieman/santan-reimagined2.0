c851bfbbce99d2d334d8077c4e4d74b31e8f1f85
ttieman <tiemantanner@gmail.com>
2023-02-24 16:15:02 -0700

    updated tiles to appropriate testing amounts

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Hero, Navbar, Card, Tabs, Heading, Container, Notification, Tile, Image, Block, Box, Section, Columns, Sizes } from "react-bulma-components";
 import PageNav from "./navbar";
 import 'bulma/css/bulma.min.css';
+import { Tab } from "bootstrap";
 
 const Menu = () => {
     return (
@@ -12,6 +13,7 @@ const Menu = () => {
                 <Box>
                     <Tile kind="ancestor" justifyContent='center'>
                         <Tile size={7} vertical >
+                   
                             <Tile   justifyContent='center'>
 
 
@@ -88,57 +90,6 @@ const Menu = () => {
                                     </Tile>
                                 </Tile>
                                 
-                                
-                            </Tile>
-                            <Tile   justifyContent='center'>
-
-
-                                <Tile kind="parent">
-                                    <Tile kind="child"  renderAs={Notification} color="info">
-                                        <Heading>Middle Tile...</Heading>
-                                        <Heading subtitle>With image Tile...</Heading>
-                                        <Image
-                                            size="4by3"
-                                            src="http://bulma.io/images/placeholders/640x480.png"
-                                        />
-                                    </Tile>
-                                </Tile>
-                                <Tile kind="parent">
-                                    <Tile kind="child" justifyContent="space-between" renderAs={Notification} color="info">
-                                        <Heading>Middle Tile...</Heading>
-                                        <Heading subtitle>With image Tile...</Heading>
-                                        <Image
-                                            size="4by3"
-                                            src="http://bulma.io/images/placeholders/640x480.png"
-                                        />
-                                    </Tile>
-                                </Tile>
-                                
-                                
-                            </Tile>
-                            <Tile   justifyContent='center'>
-
-
-                                <Tile kind="parent">
-                                    <Tile kind="child"  renderAs={Notification} color="info">
-                                        <Heading>Middle Tile...</Heading>
-                                        <Heading subtitle>With image Tile...</Heading>
-                                        <Image
-                                            size="4by3"
-                                            src="http://bulma.io/images/placeholders/640x480.png"
-                                        />
-                                    </Tile>
-                                </Tile>
-                                <Tile kind="parent">
-                                    <Tile kind="child" justifyContent="space-between" renderAs={Notification} color="info">
-                                        <Heading>Middle Tile...</Heading>
-                                        <Heading subtitle>With image Tile...</Heading>
-                                        <Image
-                                            size="4by3"
-                                            src="http://bulma.io/images/placeholders/640x480.png"
-                                        />
-                                    </Tile>
-                                </Tile>
                                 
                                 
                             </Tile>
