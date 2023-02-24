9a78ccd0b5f6521e6c1a521681ddff76c1365a9c
quetc_ <carter@epicpro.co>
2023-02-24 13:33:20 -0800

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
+import Menu from './components/menu';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" />
       </Routes>
     </Router>
   );
