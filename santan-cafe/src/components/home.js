16bb4b6934cc94888c4b96bfe7f018db6a6da3e2
quetc_ <carter@epicpro.co>
2023-02-24 12:48:16 -0800

    bulma/react hybrid testing

---
+++ santan-cafe/src/components/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Hero, Box, Block, Columns } from "react-bulma-components";
+
+import "bulma/css/bulma.min.css";
+import PageNav from "./navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <PageNav />
+      </div>
+  );
+};
+
+export default Home;
